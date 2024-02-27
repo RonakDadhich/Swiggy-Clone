e2580a575162f30f08c9266f5347d234eb04da73
Ronak Dadhich <dronak970@gmail.com>
2024-02-27 16:29:32 +0530

    first commit

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -28,12 +28,12 @@ const RestaurantMenu = () => {
     
     if(resInfo === null) return <Shimmer/>
     // const data = resInfo?.cards[0]?.card?.card?.info;
-    const {name,cuisines,costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
+    const {name,cuisines,costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
     // console.log(cuisines,costForTwoMessage)
 
-    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
+    const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
     // console.log(itemCards)
-    const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
     // console.log(categories) 
 
     if(!itemCards) return
--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -2,7 +2,7 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Emblem.png"; //faltu
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8149786&lng=75.81533859999999&restaurantId=";
+export const MENU_API = "https://corsproxy.org/?" + encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8149786&lng=75.81533859999999&restaurantId=");
 
 export const Swiggy_API = "https://corsproxy.org/?" + 
 encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8149786&lng=75.81533859999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
